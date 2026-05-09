738702623735f59ef0843282fc800113d4a39f81
David Andrei Mateiș <99895798+mateiash@users.noreply.github.com>
2026-05-09 17:47:35 +0300

    Added gravity and tracing

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import math
 
 import utils
 
+from collections import deque
+
 pygame.init()
 screen = pygame.display.set_mode((utils.SCREEN_SIZE, utils.SCREEN_SIZE))
 clock = pygame.time.Clock()
@@ -11,12 +13,29 @@ running = True
 
 positions = np.zeros((utils.N, 2))
 velocities = np.zeros((utils.N, 2))
+masses = np.zeros(utils.N)
 
 for i in range(0, utils.N):
     positions[i] = utils.pol2cart(0.5, i*(2*math.pi/utils.N))
+    # velocities[i] = (np.zeros(2) - positions[i]) * utils.INIT_SPEED
+    velocities[i] = utils.pol2cart(utils.INIT_SPEED, math.pi/2 + i*(2*math.pi/utils.N))
+    masses[i] = utils.BODY_MASS
 
 print("positions")
 print(positions)
+
+print(
+    utils.calcattractions(
+        positions, masses
+    )
+)
+
+traces = []
+
+for el in range(0, utils.N):
+    traces.append(deque([]))
+    for i in range(0, utils.POINTS_ON_LINE):
+        traces[el].append(utils.cart2screen(positions[el][0],positions[el][1]))
 
 while running:
     dt = clock.tick(60) / 1000
@@ -25,11 +44,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    accelerations = utils.calcattractions(positions, masses)
+    velocities += accelerations * dt
+    positions += velocities * dt
+
     screen.fill("black")
 
     for i in range(0, utils.N):
         pos = pygame.Vector2(utils.cart2screen(positions[i][0], positions[i][1]))
         pygame.draw.circle(screen, "white", pos, utils.BODY_SIZE)
+
+    for i in range(0, utils.N):
+        traces[i].popleft()
+        traces[i].append(utils.cart2screen(positions[i][0],positions[i][1]))
+        #print(traces[i])
+        pygame.draw.lines(screen, "gray", False, traces[i], width=2)
 
     pygame.display.flip()
 
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,19 @@
 import numpy as np
+import scipy
 
-N = 13
+#from scipy.constants import G as G
+
+G = scipy.constants.G * 1e8
+
+N = 18
+
 BODY_SIZE = 15
+BODY_MASS = 1
+
 SCREEN_SIZE = 720
+INIT_SPEED = 0.1
+
+POINTS_ON_LINE = 450
 
 def cart2screen(x, y):
     return(
@@ -19,3 +30,21 @@ def pol2cart(rho, phi):
     x = rho * np.cos(phi)
     y = rho * np.sin(phi)
     return(x, y)
+
+def calcattractions(positions, masses):
+    diff = positions[:, np.newaxis, :] - positions[np.newaxis, :, :]
+
+    distances = np.linalg.norm(diff, axis=-1)
+
+    with np.errstate(invalid='ignore'):
+        unit = diff / distances[:, :, np.newaxis]
+    unit[np.isnan(unit)] = 0
+
+    with np.errstate(divide='ignore'):
+        inv_sq = np.where(distances == 0, 0, 1 / distances ** 2)
+
+    inv_sq_weighted = inv_sq * masses[np.newaxis, :]
+
+    accelerations = -G * (inv_sq_weighted[:, :, np.newaxis] * unit).sum(axis=1)
+
+    return accelerations
