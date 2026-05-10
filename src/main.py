e5ce10d64e0403af7caaf8e89f0e187c7bffaafe
David Andrei Mateiș <99895798+mateiash@users.noreply.github.com>
2026-05-10 12:34:07 +0300

    Switched to leapfrog integration

--- src/main.py
+++ src/main.py
@@ -32,6 +32,15 @@ for el in range(0, utils.N):
     for i in range(0, utils.POINTS_ON_LINE):
         traces[el].append(utils.cart2screen(positions[el][0],positions[el][1]))
 
+def aggregate_accelerations():
+    accelerations = utils.calcattractions(positions, masses)
+    accelerations += np.array([utils.field(x, y) for x, y in positions])
+    
+    if utils.POINT_FIELD_ENABLED:
+        accelerations += np.array([utils.pointfield(x, y) for x, y in positions])
+
+    return accelerations
+
 while running:
     dt = clock.tick(60) / 1000
 
@@ -44,17 +53,13 @@ while running:
                 print(f"point field toggled: {utils.POINT_FIELD_ENABLED}")
         
 
-    accelerations = utils.calcattractions(positions, masses)
-    accelerations += np.array([utils.field(x, y) for x, y in positions])
-    
-    if utils.POINT_FIELD_ENABLED:
-        accelerations += np.array([utils.pointfield(x, y) for x, y in positions])
-    
+    positions += velocities * dt
+    accelerations = aggregate_accelerations()
     velocities += accelerations * dt
+    
     for i in range(0, utils.N):
         if(np.linalg.norm(velocities[i]) >= utils.DECAY_TRESHOLD):
             velocities[i] *= (1 - utils.SPEED_DECAY)
-    positions += velocities * dt
 
     screen.fill("black")
 
